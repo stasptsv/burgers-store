b1fa563dc9571dd1636be6636f77323d7bb89565
Stanislav Patlasov <staspatlasov19@gmail.com>
2019-01-28 21:11:50 +0500

    init

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var syntax        = 'scss'; // Syntax: sass or scss;
+
+var gulp          = require('gulp'),
+		sass          = require('gulp-sass'),
+		browserSync   = require('browser-sync'),
+		concat        = require('gulp-concat'),
+		uglify        = require('gulp-uglify'),
+		cleancss      = require('gulp-clean-css'),
+		rename        = require('gulp-rename'),
+		autoprefixer  = require('gulp-autoprefixer'),
+		sourcemaps 		=	require('gulp-sourcemaps'),
+		notify        = require('gulp-notify');
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		// open: false,
+		// online: false, // Work Offline Without Internet Connection
+		// tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
+	})
+});
+
+gulp.task('styles', function() {
+	return gulp.src('app/'+syntax+'/**/*.'+syntax+'')
+	.pipe(sourcemaps.init())
+	.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
+	.pipe(rename({ suffix: '.min', prefix : '' }))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.stream())
+});
+
+gulp.task('scripts', function() {
+	return gulp.src([
+		'app/js/main.js' // Always at the end
+		])
+	.pipe(sourcemaps.init())
+	.pipe(concat('main.min.js'))
+	.pipe(uglify()) // Mifify js (opt.)
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest('app/js'))
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('code', function() {
+	return gulp.src('app/*.html')
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('watch', function() {
+	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', gulp.parallel('styles'));
+	gulp.watch(['libs/**/*.js', 'app/js/common.js'], gulp.parallel('scripts'));
+	gulp.watch('app/*.html', gulp.parallel('code'))
+});
+gulp.task('default', gulp.parallel('styles', 'scripts', 'browser-sync', 'watch'));
+
